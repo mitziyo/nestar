ff6ba3270b7c3ecbac14f79356c652ef50388699
khan <ziyodullokhan009@mail.ru>
2026-09-09 12:04:17 +0900

    feat: Create Web Socket connection process

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -5,6 +5,7 @@ import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
 import path from 'path';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule); // AppModule => EXPRESS + NESTJS
@@ -13,6 +14,9 @@ async function bootstrap() {
 	app.enableCors({ origin: true, credentials: true });
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
 	app.use('/uploads', express.static('uploads'));
+
+	app.useWebSocketAdapter(new WsAdapter(app));
+
 	await app.listen(process.env.PORT_API ?? 3000); // portga listen qildryapmz
 }
 bootstrap();
